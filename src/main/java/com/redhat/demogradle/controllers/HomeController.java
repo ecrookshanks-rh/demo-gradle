3e8e0d3662414101687810cec879e0da70da327a
Ed Crookshanks <ecrooksh@redhat.com>
2022-11-20 10:10:09 -0500

    added helm and updated controller

--- src/main/java/com/redhat/demogradle/controllers/HomeController.java
+++ src/main/java/com/redhat/demogradle/controllers/HomeController.java
@@ -1,14 +1,18 @@
 package com.redhat.demogradle.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${output.message}")
+    private String message;
+
     @RequestMapping("/")
     public String home() {
-        String result = "Hello OpenShift World!\nSecond time around\n";
+        String result = this.message + " Third time around";
         return result;
     }
 
